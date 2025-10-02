ba61d9ae6011db8a2558ec44f823eed3823dbd7d
krzysztofrz <34165654+krzysztofrz@users.noreply.github.com>
2025-10-02 11:15:48 +0200

    player name to-do

--- 07-tic-tac-toe-starting-project/src/components/Player.jsx
+++ 07-tic-tac-toe-starting-project/src/components/Player.jsx
@@ -1,7 +1,6 @@
 export const Player = ({ name, symbol }) => {
 	return (
 		<li className="player">
-			<span className="player-name">{name}</span>
 			<span className="player-symbol">{symbol}</span>
 			<button>Edit</button>
 		</li>
